0a474b6ec6c4418801b7235fda63443fcf068512
Konstantin Sukharev <poed@me.com>
2016-11-04 17:50:19 +0300

    Hurr durr

--- Carthage/Checkouts/Fx/Source/Application+Composition.swift
+++
@@ -1,9 +0,0 @@
-/// Function application
-public func § <A, B> (f: (A) -> B, x: A) -> B {
-	return f(x)
-}
-
-/// Function composition
-public func • <A, B, C> (f: @escaping (B) -> C, g: @escaping (A) -> B) -> (A) -> C {
-	return { x in f(g(x)) }
-}
